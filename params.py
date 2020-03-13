bdd07df749918dcca0b07194c1bb3e2da5d689a7
Brian Leonard <brian.leonard@mongodb.com>
2020-03-13 12:14:35 -0400

    params clarification

--- params.py
+++ params.py
@@ -3,7 +3,7 @@
 # Target Cluster (for inspection)
 target_conn_string = '<Application connection string of target cluster>'
 
-# Results Database
+# Results Cluster and Database
 results_conn_string = '<Application connection string for results cluster>'
 results_database = 'db_stats'
 
